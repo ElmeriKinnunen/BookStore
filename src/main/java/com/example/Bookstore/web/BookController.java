625929be2d2f08995b4cef119937d444574e9447
Elmeri Kinnunen <elmeri.kin@gmail.com>
2019-02-23 10:06:54 +0200

    REST things

--- src/main/java/com/example/Bookstore/web/BookController.java
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -1,11 +1,15 @@
 package com.example.Bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.Bookstore.domain.Book;
 import com.example.Bookstore.domain.BookRepository;
@@ -24,6 +28,19 @@ public class BookController {
 		model.addAttribute("books", brepository.findAll());
 		return "booklist";
 	}
+	
+	// REST service for all
+	@RequestMapping(value="/books",  method = RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest(){
+		return (List<Book>) brepository.findAll();
+	}
+	
+	//REST service for one id
+	@RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+	public @ResponseBody Optional<Book> findBookById(@PathVariable("id")Long bookId) {
+		return brepository.findById(bookId);
+	}
+	
 	
 	@RequestMapping(value="/add")
 	public String addBook(Model model) {
